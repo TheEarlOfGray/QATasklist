838386ab080c3353dfc22dd8ab948e803947565a
Earl Gray <theearlofgray@protonmail.com>
2021-06-29 15:11:43 +0100

    Solution 3

--- solutions.sql
+++ solutions.sql
@@ -8,8 +8,14 @@ USE world;
 -- FROM country 
 -- WHERE Name = 'Argentina';
 
-SELECT Name, LifeExpectancy
-FROM country
-WHERE LifeExpectancy IS NOT NULL
-ORDER BY LifeExpectancy DESC
-LIMIT 1;
+-- SELECT Name, LifeExpectancy
+-- FROM country
+-- WHERE LifeExpectancy IS NOT NULL
+-- ORDER BY LifeExpectancy DESC
+-- LIMIT 1;
+
+SELECT city.Name 
+FROM city
+JOIN country ON city.CountryCode=country.Code
+WHERE country.Capital = city.ID AND country.Name = 'Spain';
+
