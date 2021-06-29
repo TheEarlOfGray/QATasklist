3c1a101a497eb9fead47d5d82245895091590256
Earl Gray <theearlofgray@protonmail.com>
2021-06-29 13:05:00 +0100

    Solution 3

--- solutions.sql
+++ solutions.sql
@@ -4,6 +4,12 @@ USE world;
 -- FROM city
 -- WHERE CountryCode = 'USA';
 
-SELECT Population, LifeExpectancy 
-FROM country 
-WHERE Name = 'Argentina';
+-- SELECT Population, LifeExpectancy 
+-- FROM country 
+-- WHERE Name = 'Argentina';
+
+SELECT Name, LifeExpectancy
+FROM country
+WHERE LifeExpectancy IS NOT NULL
+ORDER BY LifeExpectancy DESC
+LIMIT 1;
